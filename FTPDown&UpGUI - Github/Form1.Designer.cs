0914c443cebb332ce7e96c9f230072e01a6faa86
CatalystPro <89229819+CatalystPro@users.noreply.github.com>
2022-09-14 18:27:04 -0600

    Add files via upload

---
+++ FTPDown&UpGUI - Github/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace FTPDownloadGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.IbIStatus2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUserName2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(128, 15);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(595, 26);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User Name:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(125, 61);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(119, 26);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(359, 61);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(119, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(604, 61);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(119, 26);
+            this.txtFileName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "File Name:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(128, 122);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(598, 32);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(124, 172);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(68, 20);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Status...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 303);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtUrl);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lblStatus);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.txtUserName);
+            this.tabPage1.Controls.Add(this.txtFileName);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.txtPassword);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 270);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Download";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.IbIStatus2);
+            this.tabPage2.Controls.Add(this.progressBar);
+            this.tabPage2.Controls.Add(this.txtPassword2);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.txtUserName2);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.txtServer);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.btnUpload);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 270);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Upload";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // IbIStatus2
+            // 
+            this.IbIStatus2.AutoSize = true;
+            this.IbIStatus2.Location = new System.Drawing.Point(318, 227);
+            this.IbIStatus2.Name = "IbIStatus2";
+            this.IbIStatus2.Size = new System.Drawing.Size(105, 20);
+            this.IbIStatus2.TabIndex = 18;
+            this.IbIStatus2.Text = "Uploaded 0%";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(60, 178);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(649, 30);
+            this.progressBar.TabIndex = 17;
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.Location = new System.Drawing.Point(168, 109);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.PasswordChar = '*';
+            this.txtPassword2.Size = new System.Drawing.Size(286, 26);
+            this.txtPassword2.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Password:";
+            // 
+            // txtUserName2
+            // 
+            this.txtUserName2.Location = new System.Drawing.Point(168, 64);
+            this.txtUserName2.Name = "txtUserName2";
+            this.txtUserName2.Size = new System.Drawing.Size(286, 26);
+            this.txtUserName2.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "User Name:";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(168, 23);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(286, 26);
+            this.txtServer.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Server:";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(508, 104);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(110, 36);
+            this.btnUpload.TabIndex = 10;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click_1);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork_1);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged_1);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label IbIStatus2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtPassword2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUserName2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ FTPDown&UpGUI - Github/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FTPDownloadGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => FileDownload());
+        }
+
+        private void FileDownload()
+        {
+            try
+            {
+                NetworkCredential credentials = new NetworkCredential(txtUserName.Text, txtPassword.Text);
+                lblStatus.BeginInvoke(new Action(() =>
+                {
+                    lblStatus.Text = "Downloading...";
+                }));
+                WebRequest sizeRequest = WebRequest.Create(txtUrl.Text);
+                sizeRequest.Credentials = credentials;
+                sizeRequest.Method = WebRequestMethods.Ftp.GetFileSize;
+                int size = (int)sizeRequest.GetResponse().ContentLength;
+                progressBar1.Invoke((MethodInvoker)(() => progressBar1.Maximum = size));
+                WebRequest request = WebRequest.Create(txtUrl.Text);
+                request.Credentials = credentials;
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                using (Stream ftpStream = request.GetResponse().GetResponseStream())
+                {
+                    using (Stream fileStream = File.Create($"{Application.StartupPath}\\{txtFileName.Text}"))
+                    {
+                        byte[] buffer = new byte[10240];
+                        int read;
+                        while ((read = ftpStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fileStream.Write(buffer, 0, read);
+                            int position = (int)fileStream.Position;
+                            progressBar1.BeginInvoke(new Action(() =>
+                            {
+                                progressBar1.Value = position;
+                            }));
+                            if (position == size)
+                            {
+                                lblStatus.BeginInvoke(new Action(() =>
+                                {
+                                    lblStatus.Text = "Finished";
+                                }));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        struct FtpSetting
+        {
+            public string Server { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public string FileName { get; set; }
+            public string FullName { get; set; }
+
+        }
+
+        FtpSetting _inputParameter;
+        
+
+        private void btnUpload_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "All files|*.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi = new FileInfo(ofd.FileName);
+                    _inputParameter.UserName = txtUserName2.Text;
+                    _inputParameter.Password = txtPassword2.Text;
+                    _inputParameter.Server = txtServer.Text;
+                    _inputParameter.FileName = fi.Name;
+                    _inputParameter.FullName = fi.FullName;
+                    backgroundWorker1.RunWorkerAsync(_inputParameter);
+                }
+            }
+        }
+
+        private void backgroundWorker1_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+            string fileName = ((FtpSetting)e.Argument).FileName;
+            string fullName = ((FtpSetting)e.Argument).FullName;
+            string userName = ((FtpSetting)e.Argument).UserName;
+            string password = ((FtpSetting)e.Argument).Password;
+            string server = ((FtpSetting)e.Argument).Server;
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}", server, fileName)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(userName, password);
+            Stream ftpStream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(fullName);
+            byte[] buffer = new byte[1024];
+            double total = (double)fs.Length;
+            int byteRead = 0;
+            double read = 0;
+            do
+            {
+                if (!backgroundWorker1.CancellationPending)
+                {
+                    byteRead = fs.Read(buffer, 0, 1024);
+                    ftpStream.Write(buffer, 0, byteRead);
+                    read += (double)byteRead;
+                    double percentage = read / total * 100;
+                    backgroundWorker1.ReportProgress((int)percentage);
+                }
+            }
+            while (byteRead != 0);
+            fs.Close();
+            ftpStream.Close();
+        }
+
+        private void backgroundWorker1_ProgressChanged_1(object sender, ProgressChangedEventArgs e)
+        {
+            IbIStatus2.Text = $"Uploaded {e.ProgressPercentage} %";
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Update();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted_1(object sender, RunWorkerCompletedEventArgs e)
+        {
+            IbIStatus2.Text = "Upload Completed ! ";
+        }
+
+    }
+}
